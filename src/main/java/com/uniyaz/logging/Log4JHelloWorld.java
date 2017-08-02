9c4f53cb1d347d19abe038af1de4f485f5478d5e
yimu187 <yimu187@gmail.com>
2017-08-02 15:55:28 +0300

    Commit

---
+++ src/main/java/com/uniyaz/logging/Log4JHelloWorld.java
@@ -0,0 +1,23 @@
+package com.uniyaz.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Murat on 7/31/2017.
+ */
+
+public class Log4JHelloWorld {
+
+    private final static Logger logger = LoggerFactory.getLogger(Log4JHelloWorld.class);
+
+    public static void main(String[] args){
+        logger.debug("Hello World!");
+
+        logger.debug("Sample debug message");
+        logger.info("Sample info message");
+        logger.error("Sample error message");
+
+        logger.info("Sample with params. param1: {} param2: {}", 100, 101);
+    }
+}
